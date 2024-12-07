0a8de5aa42df8863ae3cf957c2dc428b107378dd
josue130 <josuelopez67i90@gmail.com>
2024-12-07 16:55:35 -0600

    feat: add error messages

---
+++ GameRaitingAPI/Validations/ErrorMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace GameRaitingAPI.Validations
+{
+    public static class ErrorMessages
+    {
+        public static string FieldIsRequired = "The {PropertyName} field is required";
+        public static string MaximumLength = "The {PropertyName} field must be less than {MaxLength} characters";
+        public static string FirstCapitalLetter = "The first letter must be capitalized";
+        public static string GenreAlreadyExist = "Genre already exist";
+    }
+}
