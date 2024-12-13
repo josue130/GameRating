89cc0e71f4e894935e0afb0aae587f2dea74080a
josue130 <josuelopez67i90@gmail.com>
2024-12-12 18:03:42 -0600

    feat: add rating repository

---
+++ GameRaitingAPI/Repository/IRepository/IRatingRepository.cs
@@ -0,0 +1,12 @@
+﻿using GameRaitingAPI.Entitie;
+
+namespace GameRaitingAPI.Repository.IRepository
+{
+    public interface IRatingRepository
+    {
+        Task<int> Add(Rating rating);
+        Task<Rating?> Exist(string userId, int gameId);
+        Task Save();
+
+    }
+}
---
+++ GameRaitingAPI/Repository/RatingRepository.cs
@@ -0,0 +1,41 @@
+﻿using GameRaitingAPI.Entitie;
+using GameRaitingAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameRaitingAPI.Repository
+{
+    public class RatingRepository : IRatingRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RatingRepository(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+        public async Task<int> Add(Rating rating)
+        {
+            var game = await _context.games.FirstOrDefaultAsync(g => g.Id == rating.GameId);
+
+            if (game is null)
+            {
+                throw new ArgumentException($"Game with {rating.Id} not found");
+            }
+
+            await _context.AddAsync(rating);
+
+            await Save();
+
+
+            return rating.Id;
+        }
+        public async Task<Rating?> Exist(string userId, int gameId)
+        {
+            return await _context.ratings.FirstOrDefaultAsync(r => r.GameId == gameId && r.UserId == userId);
+        }
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
