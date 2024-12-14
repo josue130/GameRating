ec2870e410f10d6b96e2b2658f99d14162840dc4
josue130 <josuelopez67i90@gmail.com>
2024-12-13 20:50:21 -0600

    feat: add azure storage

---
+++ GameRaitingAPI/Services/AzureStorage.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using GameRaitingAPI.Services.IServices;
+
+namespace GameRaitingAPI.Services
+{
+    public class AzureStorage : IImageStorage
+    {
+        private string connection;
+        public AzureStorage(IConfiguration configuration)
+        {
+            connection = configuration.GetConnectionString("AzureStorage")!;
+        }
+        public async Task Delete(string? route, string container)
+        {
+            if (string.IsNullOrEmpty(route))
+            {
+                return;
+            }
+
+            var client = new BlobContainerClient(connection, container);
+            await client.CreateIfNotExistsAsync();
+            var fileName = Path.GetFileName(route);
+            var blob = client.GetBlobClient(fileName);
+            await blob.DeleteIfExistsAsync();
+        }
+
+        public async Task<string> Store(string container, IFormFile file)
+        {
+            var client = new BlobContainerClient(connection, container);
+            await client.CreateIfNotExistsAsync();
+            client.SetAccessPolicy(PublicAccessType.Blob);
+
+            var extension = Path.GetExtension(file.FileName);
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            var blob = client.GetBlobClient(fileName);
+            var blobHttpHeaders = new BlobHttpHeaders();
+            blobHttpHeaders.ContentType = file.ContentType;
+            await blob.UploadAsync(file.OpenReadStream(), blobHttpHeaders);
+            return blob.Uri.ToString();
+        }
+    }
+}
